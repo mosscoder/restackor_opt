de1edae87735a448b53ac63e8cce00d05b33e5d2
Kyle David Doherty <kyledaviddoherty@gmail.com>
2022-02-03 20:40:59 -0700

    add to codebase

---
+++ ro_funs.R
@@ -0,0 +1,145 @@
+restackor_xls <- 'C:\\ReStackor\\Excel\\metric-ReStackor.xls'
+restack_csv <- 'C:\\ReStackor\\Work_Dir\\restackor.csv'
+restackor_exec <- 'C:\\ReStackor\\Code\\restackor.exe'
+
+estimate_coil_rate <- function(d_wire, d_coil, n_active){
+  top <- 8080*(d_wire^4)
+  bottom <- (8*(d_coil - d_wire)^3)*n_active
+  top/bottom
+}
+
+set_fixed_params <- function(shim_id = NULL,
+                       float = NULL,
+                       d_rod = NULL,
+                       d_valve = NULL,
+                       w_seat = NULL,
+                       v_spec = NULL,
+                       r_port = NULL,
+                       d_port = NULL,
+                       w_port = NULL,
+                       n_port = NULL,
+                       h_deck = NULL,
+                       d_leak = NULL,
+                       d_throat = NULL,
+                       n_throat = NULL,
+                       d_bleed = NULL,
+                       max_click = NULL,
+                       d_hsc = NULL,
+                       k_spring = NULL
+                       ) {
+  
+  wb <- XLConnect::loadWorkbook(restackor_xls_loc)
+  
+  excel_map <- 
+    list(si = c(shim_id, 6, 3), 
+         f = c(float, 6, 4), 
+         dr = c(d_rod, 4, 8), 
+         dv = c(d_valve, 4, 9),  
+         ws = c(w_seat, 4, 10),
+         vs = c(v_spec, 4, 11),
+         rp = c(r_port, 6, 8), 
+         dp = c(d_port, 6, 9), 
+         wp = c(w_port, 6, 10 ),
+         np = c(n_port, 6, 11),
+         hd = c(h_deck, 8, 8),
+         dl = c(d_leak, 8, 9),
+         dt = c(d_throat, 8, 10),
+         nt = c(n_throat, 8, 11),
+         db = c(d_bleed, 4, 14),
+         mc = c(max_click, 4, 15),
+         dh = c(d_hsc, 6, 15),
+         k = c(k_spring, 6, 16)
+         )
+  
+  for(i in names(excel_map)){
+    focal_prop <- excel_map[[i]]
+    
+    if(!is.null(focal_prop[1])) { 
+      
+    XLConnect::writeWorksheet(
+      object = wb,
+      data = focal_prop[1],
+      startRow = focal_prop[2],
+      startCol = focal_prop[3],
+      sheet = 'Plots',
+      header = F
+    )
+      
+      }
+  }
+  
+  XLConnect::saveWorkbook(wb)
+  
+}
+
+load_database_params <- function(damper_name){
+  dloc <- tempfile()
+  download.file('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ6d6Dh3Yz9j4ANKedh1fBz0g-LsnqcaASWh3VeZQqyVIrVj0EJDV8DRyAMbqtg0DpE8Ndlde3hi4Rf/pub?output=csv',
+                destfile = dloc)
+  
+  focal_props <- read.csv(dloc)[, damper_name]
+  set_fixed_params(focal_props)
+}
+
+set_adjusters <- function(n_click = NULL,
+                          f_max = NULL,
+                          u_wheel = 8,
+                          preload = NULL) {
+  wb <- XLConnect::loadWorkbook(restackor_xls_loc)
+  
+  excel_map <-
+    list(
+      nc = c(n_click, 11, 8),
+      fm = c(f_max, 11, 9),
+      uw = c(u_wheel, 11, 10),
+      p = c(pre, 6, 14)
+    )
+  
+  for (i in names(excel_map)) {
+    focal_prop <- excel_map[[i]]
+    
+    if (!is.null(focal_prop[1])) {
+      XLConnect::writeWorksheet(
+        object = wb,
+        data = focal_prop[1],
+        startRow = focal_prop[2],
+        startCol = focal_prop[3],
+        sheet = 'Plots',
+        header = F
+      )
+      
+    }
+  }
+  
+  XLConnect::saveWorkbook(wb)
+  
+}
+                    
+run_shimstack <- function(shim_df, outfile) {
+  wb <- XLConnect::loadWorkbook(restackor_xls)
+  
+  XLConnect::clearRange(object = wb,
+             sheet = 'Plots',
+             coords = c(9, 3, 58, 4))
+  
+  XLConnect::writeWorksheet(
+    object = wb,
+    data = shim_df,
+    startRow = 9,
+    startCol = 3,
+    sheet = 'Plots',
+    header = F
+  )
+  
+  XLConnect::saveWorkbook(wb)
+  
+  system(restackor_exec)
+  
+  file.copy(from = restackor_exec,
+            to = outfile)
+}
+
+make_add_profile <- function(start_shim, end_shim, shim_count, shape){
+  raw_widths <- dweibull(seq_len(shim_count), shape = shape, scale = 1, log = TRUE)
+  round(scales::rescale(raw_widths, to = c(end_shim, start_shim)))
+}
---
+++ scratch.R
@@ -0,0 +1,54 @@
+source('')
+
+stack_1 <- c(18, 17, 16, 15, 14, 13, 12, 11, 10)
+stack_2 <- c(18, 18, 18, 18, 16, 14, 12, 11, 10)
+stack_3 <- c(18, 15, 13, 11, 10, 10, 10, 10, 10)
+
+examps <- data.frame(stack_1, stack_2, stack_3) %>% 
+  mutate(id = row_number()) %>% 
+  pivot_longer(stack_1:stack_3, names_to = 'stack', values_to = 'width')
+
+ggplot(examps, aes(x = factor(id), y = width)) +
+  geom_bar(stat = 'identity') +
+  theme_classic() +
+  facet_wrap(~stack)
+
+tg <- expand.grid(
+  shim_count = 2:12,
+  shim_thickness = 0.1,
+  start_width = 18,
+  end_width = c(10:18),
+  shape =  c(0,0.1, 0.5, 1, 2, 3, 5, 10),
+  clamp_width = 8:15,
+  clamp_thickness = c(0.2, 0.4)
+)
+
+build_stack_from_grid <- function(tune_grid, x){
+  focal_params <- tune_grid[x, ]
+  if(focal_params$shape != 0){
+    base_stack <- make_add_profile(start_shim = focal_params$start_width,
+                     end_shim = focal_params$end_width,
+                     shim_count = focal_params$shim_count,
+                     shape = focal_params$shape
+    )
+  } else {
+    base_stack <- rep(focal_params$start_width, focal_params$shim_count)
+  }
+  
+  if(tail(base_stack, 1) <= focal_params$clamp_width)
+    return(NA)
+  
+  width <- c(base_stack, focal_params$clamp_width)
+  thickness <- c(rep(focal_params$shim_thickness, 
+                     focal_params$shim_count),
+                   focal_params$clamp_thickness)
+  
+  return(cbind(width, thickness))
+  
+}
+
+result <- lapply(FUN = build_stack_from_grid, X = seq_len(nrow(tune_grid)), tune_grid = tg)
+keeper_inds <- which(!duplicated(result) & !is.na(result))
+keeper_params <- tune_grid[keeper_inds,]
+finals <- result[keeper_inds]
+
