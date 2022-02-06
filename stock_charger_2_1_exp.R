ab42887279887d8f98515a79184edc97bd34b71c
Kyle David Doherty <kyledaviddoherty@gmail.com>
2022-02-06 00:29:51 -0700

    add candidate

--- stock_charger_2_1_exp.R
+++ stock_charger_2_1_exp.R
@@ -14,28 +14,42 @@ set_adjusters(n_click = 9, # set the adjusters
 stock_charger_stack <- data.frame(width = c(18, 16, 14, 8, 10), 
                                   thickness = c(0.1, 0.1, 0.1, 0.4, 4))
 
+candidate_stack <- data.frame(width = c(18, 18, 18, 16, 14, 8, 10), 
+                                  thickness = c(0.1, 0.1, 0.1, 0.1, 0.1, 0.4, 4))
+
 # run restackor.exe
 run_shimstack(shim_df = stock_charger_stack,
               outfile = file.path(results_loc, 'stock_charger.csv'))
 
+run_shimstack(shim_df = candidate_stack,
+              outfile = file.path(results_loc, 'candidate.csv'))
+
+
 #plot outcome
-result <- tidy_restackor_results(file.path(results_loc, 'stock_charger.csv'))
+stock_result <- tidy_restackor_results(file.path(results_loc, 'stock_charger.csv')) %>% 
+  pivot_longer(cols = U.clk:U.clsd,
+               names_to = 'setting',
+               values_to = 'velocity') %>% 
+  mutate(Stack = 'Stock charger')
+
+candidate_result <- tidy_restackor_results(file.path(results_loc, 'candidate.csv')) %>% 
+  pivot_longer(cols = U.clk:U.clsd,
+               names_to = 'setting',
+               values_to = 'velocity') %>% 
+  mutate(Stack = 'Candidate')
 
 png(file.path(results_loc, 'stock_charger_force_plot.png'), 
     height = 4, width = 8, res = 200, units = 'in')
-ggplot(result %>% 
-         pivot_longer(cols = U.clk:U.clsd,
-                      names_to = 'setting',
-                      values_to = 'velocity'),
-       aes(y = Fstack.1,
+ggplot(rbind(stock_result, candidate_result),
+       aes(y = Fshaft,
            x = velocity, 
            group = setting,
-           color = setting)
+           linetype = setting,
+           color = Stack)
 ) +
-  geom_line(size = 1.5, alpha = 0.6) +
-  scale_color_manual(labels = c('Middle','Closed', 'Open'), 
-                     name = 'Clicker\nsetting',
-                     values = c('black','red','blue')) +
+  geom_line(size = 1, alpha = 0.6) +
+  scale_linetype_manual(values = c(1,2,2)) +
+  guides(linetype = 'none') +
   xlab('Shaft velocity (m/sec)') +
   ylab('Force (kgf)')
 dev.off()
