0b5a8dc125449d11a9c532562082d9aec7f42bef
Kyle David Doherty <kyledaviddoherty@gmail.com>
2022-02-05 20:53:44 -0700

    add nut to charger examp

--- stock_charger_2_1_exp.R
+++ stock_charger_2_1_exp.R
@@ -12,14 +12,14 @@ set_adjusters(n_click = 9, # set the adjusters
               u_wheel = 3)
 
 # the stock charger shim stack
-stock_charger_stack <- data.frame(width = c(18, 16, 14, 8), 
-                                  thickness = c(0.1, 0.1, 0.1, 0.4))
+stock_charger_stack <- data.frame(width = c(18, 16, 14, 8, 10), 
+                                  thickness = c(0.1, 0.1, 0.1, 0.4, 4))
 # run restackor.exe
 run_shimstack(shim_df = stock_charger_stack,
-              outfile = file.path(results_loc, 'stocker_charger.csv'))
+              outfile = file.path(results_loc, 'stock_charger.csv'))
 
 #plot outcome
-result <- tidy_restackor_results(file.path(results_loc, 'stocker_charger.csv'))
+result <- tidy_restackor_results(file.path(results_loc, 'stock_charger.csv'))
 
 png(file.path(results_loc, 'stock_charger_force_plot.png'), 
     height = 5, width = 5, res = 200, units = 'in')
